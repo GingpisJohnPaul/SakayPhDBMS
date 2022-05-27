20b8cc417b5084e938f0237de7c7129cd4579e4e
Earvin Jon Arsua <201910955@gordoncollege.edu.ph>
2022-05-27 17:32:42 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,9 +13,6 @@
     <h1>Welcome to Sakay.Ph</h1>
     <p>Database Management System</p>
     <img src="{{ asset('images/sakaylogo.png') }}" alt="logo" class="logo">
-    
-
-<<<<<<< HEAD
     <form action="/home/date" method="get">
         <select name="date">
             <option value="today">Today</option>
@@ -26,24 +23,13 @@
     </form>
 <div class="card">
     <div class="card-body">
-=======
-
- 
-    
-            <div class="dashboard">
-                
->>>>>>> 809b5ad2185679a2a1b28eb21953626f89a7a498
+        <div class="dashboard">
         <div class="row">
             <div class="col-sm-4">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Total Number of Passenger</h5>
-<<<<<<< HEAD
                         <p class="card-text">{{$totalPassenger}}</p>
-=======
-                         {{--<p class="card-text">{{$total}}</p>--}}
-                        <p class="card-text">350<p>
->>>>>>> 809b5ad2185679a2a1b28eb21953626f89a7a498
                     </div>
                 </div>
             </div>
@@ -52,12 +38,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Total Number of Driver</h5>
-<<<<<<< HEAD
                         <p class="card-text">{{$totalDriver}}</p>
-=======
-                        {{--<p class="card-text">{{$total}}</p>--}}
-                        <p class="card-text">350<p>
->>>>>>> 809b5ad2185679a2a1b28eb21953626f89a7a498
                     </div>
                 </div>
             </div>
@@ -66,12 +47,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Total Number of Trips</h5>
-<<<<<<< HEAD
                         <p class="card-text">{{$totalTrips}}</p>
-=======
-                        {{--<p class="card-text">{{$total}}</p>--}}
-                        <p class="card-text">350<p>
->>>>>>> 809b5ad2185679a2a1b28eb21953626f89a7a498
                     </div>
                 </div>
             </div>
